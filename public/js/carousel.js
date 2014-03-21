0e3764ba435b64fa13973b2c778930f427d02d5b
Luke Patterson <lukewpatterson@gmail.com>
2014-03-20 22:36:39 -0500

    issue #2 - add basic drag swipe

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -59,3 +59,12 @@ jQuery(document).ready(function() {
   $(window).on('load resize orientationchange', resizeCarousel);
 
 });
+
+$(document).ready(function() {  
+      Hammer($('.carousel-inner').get(0)).on('dragleft', function() {
+        $('.right.carousel-control').click();
+      })
+      Hammer($('.carousel-inner').get(0)).on('dragright', function() {
+        $('.left.carousel-control').click();
+      })
+ });
