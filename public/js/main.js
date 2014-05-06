3ad5e5a5c6fa997c741212b854545931265d8be0
Luke Patterson <lukewpatterson@gmail.com>
2014-05-06 06:58:52 +0000

    use object-fit for letterboxing, still resizing img with main script though

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,14 @@
 jQuery(document).ready(function() {
 
-  var setSizeAndMargins = function(selector, width, height, marginTop, marginBottom, marginLeft) {
-    selector.width(width);
-    selector.height(height);
-    selector.css( {'margin-top' : marginTop + 'px', 'margin-bottom' : marginBottom + 'px', 'margin-left' : marginLeft + 'px', 'margin-right' : 'auto'} );
-    window.scrollTo(0, 1);
-  };
-
   var resize = function() {
     var windowHeight = $(window).outerHeight(),
       headerHeight = $('#header').outerHeight(),
       footerHeight = $('#footer').outerHeight(),
       contentHeight = windowHeight - headerHeight - footerHeight,
-      contentWidth = $('#content').width(),
-      contentAspectRatio = contentWidth / contentHeight;
-
-    $('img.maximized').each(function(index, element) {
-      var item = $(element),
-        optimalWidth = item.data('optimal-width'),
-        optimalHeight = item.data('optimal-height'),
-        optimalAspectRatio = optimalWidth / optimalHeight,
-        scaledWidth,
-        scaledHeight,
-        marginTop,
-        marginBottom,
-        marginLeft;
-      if (contentAspectRatio > optimalAspectRatio) { /* height will be constraining factor */
-        scaledWidth = (contentHeight / optimalHeight) * optimalWidth;
-        scaledHeight = contentHeight;
-        marginTop = marginBottom = 0;
-        marginLeft = (contentWidth - scaledWidth) / 2;
-      } else { /* width will be constraining factor */
-        scaledWidth = contentWidth;
-        scaledHeight = (contentWidth / optimalWidth) * optimalHeight;
-        marginTop = marginBottom = (contentHeight - scaledHeight) / 2;
-        marginLeft = 0;
-      }
-      setSizeAndMargins(item, scaledWidth, scaledHeight, marginTop, marginBottom, marginLeft);
-    });
-
+      contentWidth = $('#header').width(),
+      selector = $('img.maximized');
+      selector.width(contentWidth);
+      selector.height(contentHeight);
   };
 
   $(window).on('load resize orientationchange', resize);
